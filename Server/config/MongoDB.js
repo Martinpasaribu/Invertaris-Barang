3cabbcb7a51569b41a00da8f776daaddb701aab2
Martinpasaribu <83689864+Martinpasaribu@users.noreply.github.com>
2024-07-17 20:56:56 +0700

    revisi

--- Server/config/MongoDB.js
+++ Server/config/MongoDB.js
@@ -1,9 +1,5 @@
 import mongoose from "mongoose";
 
-mongoose.connect('mongodb+srv://martin12082000:Martin1208__+@labora1.g4e9sa3.mongodb.net/?retryWrites=true&w=majority&appName=Labora1',{
-    user:'martin12082000',
-    pass:'Martin1208__+',
-})
 
   export const Koneksi = mongoose.connection;
 
